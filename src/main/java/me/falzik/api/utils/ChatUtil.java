ca4e34383b25d915da3c5ec637d0e7aa6fb13f1f
Artem <almatyecofest@gmail.com>
2025-06-11 15:12:49 +0500

    Update

--- src/main/java/me/falzik/api/utils/ChatUtil.java
+++ src/main/java/me/falzik/api/utils/ChatUtil.java
@@ -4,9 +4,12 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.title.Title;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.logging.Logger;
 
@@ -15,6 +18,7 @@ public class ChatUtil {
     private static String prefix;
     private static String errorPrefix;
 
+    // Setup plugin need if you use util for config, or you need to get logger
     private static JavaPlugin plugin;
 
     public static String translateCodes(String value) {
@@ -78,5 +82,34 @@ public class ChatUtil {
 
     public static void setPlugin(JavaPlugin plugin) {
         ChatUtil.plugin = plugin;
+    }
+
+    public static Object get(String path) {
+        if(plugin == null) return null;
+        return plugin.getConfig().get(path);
+    }
+
+    public static Object get(String configName, String path) {
+        if(plugin == null) return null;
+        File file = new File(plugin.getDataFolder() + "/", path);
+        YamlConfiguration configuration = YamlConfiguration.loadConfiguration(file);
+        return configuration.get(path);
+    }
+
+    public static void set(String path, Object object) {
+        if(plugin == null) return;
+        plugin.getConfig().set(path, object);
+    }
+
+    public static void set(String configName, String path, Object object) {
+        if(plugin == null) return;
+        File file = new File(plugin.getDataFolder() + "/", configName);
+        YamlConfiguration yamlConfiguration = YamlConfiguration.loadConfiguration(file);
+        yamlConfiguration.set(path, object);
+        try {
+            yamlConfiguration.save(file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
